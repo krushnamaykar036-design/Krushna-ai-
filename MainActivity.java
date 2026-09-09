76b872002237301610272298c59c5b32af536c96
Krushna <krushnamaykar036@gmail.com>
2026-09-09 21:11:28 +0530

    Add Internet permission to AndroidManifest
    Add Internet permission to AndroidManifest.xml using sed command.

--- MainActivity.java
+++ MainActivity.java
@@ -509,3 +509,7 @@ public class MainActivity extends Activity {
         super.onDestroy();
     }
 }
+      - name: Add Internet Permission
+        run: |
+          MANIFEST="$PROJECT/app/src/main/AndroidManifest.xml"
+          sed -i '/<manifest/a\    <uses-permission android:name="android.permission.INTERNET" />' "$MANIFEST"
