1812836e8e6ffe217086db8bc6c121d9db3e66a2
Krushna <krushnamaykar036@gmail.com>
2026-09-09 21:52:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require("express");
 
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Krushna AI Server चालू आहे!");
+});
+
+app.post("/chat", (req, res) => {
+  const message = req.body.message || "";
+
+  res.json({
+    reply: "Krushna AI Server ला message मिळाला: " + message
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Krushna AI Server started");
+});
