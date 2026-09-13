d97274467bba8f7cdd6ac5145345f8b8ca2da54e
Krushna <krushnamaykar036@gmail.com>
2026-09-13 12:49:02 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
